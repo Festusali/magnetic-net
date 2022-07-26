069de038a33e13ff30e8b8fbdb974c458d36f28e
Festus Ali <festusali@gmail.com>
2022-07-26 09:43:31 +0100

    Added Customers Review  and Updated Prices

--- js/script.js
+++ js/script.js
@@ -3,25 +3,22 @@
 // Assigns curresponding value to price based on quantity
 function checkForm() {
     orderQty = document.orderForm.quantity
-    if (orderQty.value == 1) {
-        document.orderForm.price.value = "₦10,000";
-        return true;
-    }
     if (orderQty.value == 2) {
-        document.orderForm.price.value = "₦15,000";
+        document.orderForm.price.value = "₦16,000";
         return true;
     }
     if (orderQty.value == 3) {
-        document.orderForm.price.value = "₦22,000";
+        document.orderForm.price.value = "₦24,000";
         return true;
     }
     if (orderQty.value == 4) {
-        document.orderForm.price.value = "₦30,000";
+        document.orderForm.price.value = "₦32,000";
         return true;
     }
     if (orderQty.value == 5) {
-        document.orderForm.price.value = "₦35,000";
+        document.orderForm.price.value = "₦40,000";
         return true;
     }
+    alert("Choose a valid quantity to continue!");
     return false;
 }
