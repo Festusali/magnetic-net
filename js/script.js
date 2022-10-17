43ec15dd9ee3d687fd48435a121f601042a6413a
Festus Ali <42407804+Festusali@users.noreply.github.com>
2022-10-18 00:06:50 +0100

    Changed Prices and Bonus Text

--- js/script.js
+++ js/script.js
@@ -3,20 +3,24 @@
 // Assigns curresponding value to price based on quantity
 function checkForm() {
     orderQty = document.orderForm.quantity
+    if (orderQty.value == 1) {
+        document.orderForm.price.value = "₦10,000";
+        return true;
+    }
     if (orderQty.value == 2) {
-        document.orderForm.price.value = "₦16,000";
+        document.orderForm.price.value = "₦14,000";
         return true;
     }
     if (orderQty.value == 3) {
-        document.orderForm.price.value = "₦24,000";
+        document.orderForm.price.value = "₦19,500";
         return true;
     }
     if (orderQty.value == 4) {
-        document.orderForm.price.value = "₦32,000";
+        document.orderForm.price.value = "₦25,500";
         return true;
     }
     if (orderQty.value == 5) {
-        document.orderForm.price.value = "₦40,000";
+        document.orderForm.price.value = "₦32,000";
         return true;
     }
     alert("Choose a valid quantity to continue!");
