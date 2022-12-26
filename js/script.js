8f34d6f17865986b4eac6d95c3e297033c9a9791
Festus Ali <festusali@gmail.com>
2022-12-26 16:42:23 +0100

    Change Price and Reduce Items To Four

--- js/script.js
+++ js/script.js
@@ -4,29 +4,29 @@
 function checkForm() {
     orderQty = document.orderForm.quantity
     if (orderQty.value == 1) {
-        document.orderForm.price.value = "₦10,000";
+        document.orderForm.price.value = "₦17,000";
         return true;
     }
     if (orderQty.value == 2) {
-        document.orderForm.price.value = "₦15,000";
+        document.orderForm.price.value = "₦30,000";
         return true;
     }
     if (orderQty.value == 3) {
-        document.orderForm.price.value = "₦21,000";
+        document.orderForm.price.value = "₦42,000";
         return true;
     }
     if (orderQty.value == 4) {
-        document.orderForm.price.value = "₦27,500";
+        document.orderForm.price.value = "₦50,500";
         return true;
     }
-    if (orderQty.value == 5) {
-        document.orderForm.price.value = "₦33,500";
-        return true;
-    }
-    if (orderQty.value == 5) {
-        document.orderForm.price.value = "₦39,000";
-        return true;
-    }
-    alert("Choose a valid quantity to continue!");
-    return false;
+    // if (orderQty.value == 5) {
+    //     document.orderForm.price.value = "₦33,500";
+    //     return true;
+    // }
+    // if (orderQty.value == 5) {
+    //     document.orderForm.price.value = "₦39,000";
+    //     return true;
+    // }
+    // alert("Choose a valid quantity to continue!");
+    // return false;
 }
