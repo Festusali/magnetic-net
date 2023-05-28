49b4e31abf1ab53ee10635fdcd8d349e65291173
Festus Ali <festusali@gmail.com>
2023-05-28 15:20:19 +0100

    Add Support For Ghana Customers

--- js/script.js
+++ js/script.js
@@ -4,19 +4,19 @@
 function checkForm() {
     orderQty = document.orderForm.quantity
     if (orderQty.value == 2) {
-        document.orderForm.price.value = "₦17,000";
+        document.orderForm.price.value = "₦16,000/ GHC400";
         return true;
     }
     if (orderQty.value == 4) {
-        document.orderForm.price.value = "₦30,000";
+        document.orderForm.price.value = "₦30,000/ GHC780";
         return true;
     }
     if (orderQty.value == 6) {
-        document.orderForm.price.value = "₦42,000";
+        document.orderForm.price.value = "₦42,000/ GHC1140";
         return true;
     }
     if (orderQty.value == 8) {
-        document.orderForm.price.value = "₦50,500";
+        document.orderForm.price.value = "₦50,500/ GHC1480";
         return true;
     }
     // if (orderQty.value == 5) {
