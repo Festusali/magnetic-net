56d51ef1024a15013a85266f6a6c3a9f3406b4c2
Festus Ali <festusali@gmail.com>
2024-01-07 16:28:11 +0100

    Change Prices In Net Websites

--- magic/js/script.js
+++ magic/js/script.js
@@ -3,32 +3,46 @@
 // Assigns corresponding value to price based on Net quantity
 function checkForm() {
     orderQty = document.orderForm.quantity
+    if (orderQty.value == 1) {
+        document.orderForm.price.value = "₦16,000";
+        return true;
+    }
     if (orderQty.value == 2) {
-        document.orderForm.price.value = "₦17,000/ GHC400";
+        document.orderForm.price.value = "₦25,000";
+        return true;
+    }
+    if (orderQty.value == 3) {
+        document.orderForm.price.value = "₦37,000";
         return true;
     }
     if (orderQty.value == 4) {
-        document.orderForm.price.value = "₦32,000/ GHC780";
+        document.orderForm.price.value = "₦48,000";
+        return true;
+    }
+    if (orderQty.value == 5) {
+        document.orderForm.price.value = "₦58,000";
         return true;
     }
     if (orderQty.value == 6) {
-        document.orderForm.price.value = "₦45,000/ GHC1140";
+        document.orderForm.price.value = "₦69,000";
+        return true;
+    }
+    if (orderQty.value == 7) {
+        document.orderForm.price.value = "₦79,000";
         return true;
     }
     if (orderQty.value == 8) {
-        document.orderForm.price.value = "₦54,000/ GHC1480";
+        document.orderForm.price.value = "₦88,000";
+        return true;
+    }
+    if (orderQty.value == 9) {
+        document.orderForm.price.value = "₦96,000";
         return true;
     }
     if (orderQty.value == 10) {
-        document.orderForm.price.value = "₦65,000/ GHC1800";
+        document.orderForm.price.value = "₦100,000";
         return true;
     }
-    // if (orderQty.value == 5) {
-    //     document.orderForm.price.value = "₦39,000";
-    //     return true;
-    // }
-    // alert("Choose a valid quantity to continue!");
-    // return false;
 }
 
 
