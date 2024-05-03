ac4f235aad5677c7a851bcc88d3488c89a22897b
Festus Ali <festusali@gmail.com>
2024-05-03 15:01:08 +0100

    First Build Of Vista Eye Drop Website

---
+++ eye-care/assets/script.js
@@ -0,0 +1,14 @@
+// Script for managing orders for Vista Eye Drop Website
+
+// Assigns corresponding value to price based on eye drop quantity
+function checkForm() {
+    orderQty = document.orderForm.quantity
+    if (orderQty.value == 1) {
+        document.orderForm.price.value = "₦16,500";
+        return true;
+    }
+    if (orderQty.value == 2) {
+        document.orderForm.price.value = "₦33,000";
+        return true;
+    }
+}
